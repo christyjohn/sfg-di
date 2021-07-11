33b01078a6869d706de1249c0cdcfafe26b56009
Christy John <christyjohn.crz@gmail.com>
2021-07-11 16:48:36 +0530

    adding examples for Spring Profiles

--- src/main/java/me/christyjohn/sfgdi/SfgDiApplication.java
+++ src/main/java/me/christyjohn/sfgdi/SfgDiApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 
 import me.christyjohn.sfgdi.controllers.ConstructorInjectedController;
+import me.christyjohn.sfgdi.controllers.I18nController;
 import me.christyjohn.sfgdi.controllers.MyController;
 import me.christyjohn.sfgdi.controllers.PropertyInjectedController;
 import me.christyjohn.sfgdi.controllers.SetterInjectedController;
@@ -15,10 +16,14 @@ public class SfgDiApplication {
 	public static void main(String[] args) {
 		ApplicationContext ctx = SpringApplication.run(SfgDiApplication.class, args);
 		
+		I18nController i18nController = (I18nController) ctx.getBean("i18nController");
+		System.out.println(i18nController.sayHello());
+		
 		MyController myController = (MyController) ctx.getBean("myController");
 		
 		String greeting = myController.sayHello();
 		
+		System.out.println("------ Primary Bean ------");
 		System.out.println(greeting);
 		
 		System.out.println("------ Property ------");
---
+++ src/main/java/me/christyjohn/sfgdi/controllers/I18nController.java
@@ -0,0 +1,20 @@
+package me.christyjohn.sfgdi.controllers;
+
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Controller;
+
+import me.christyjohn.sfgdi.services.GreetingService;
+
+@Controller
+public class I18nController {
+
+	private final GreetingService greetingService;
+
+    public I18nController(@Qualifier("i18nService") GreetingService greetingService) {
+        this.greetingService = greetingService;
+    }
+
+    public String sayHello(){
+        return greetingService.sayGreeting();
+    }
+}
